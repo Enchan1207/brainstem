5531af539f479378c598e3b6529f892ebe7acb90
Enchan <enchant0130@gmail.com>
2025-01-01 23:27:11 +0900

    [Add] test

--- Tests/brainfuck_implTests/brainfuck_implTests.swift
+++ Tests/brainfuck_implTests/brainfuck_implTests.swift
@@ -4,3 +4,20 @@ import Testing
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
 }
+
+@Test func testRunHelloWorld() async throws {
+    class TestProgiramSource: BFProgramDataSource {
+        private let program = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."
+        
+        func opcode(at address: BFAddress) -> BFMachineOpcode? {
+            guard address < program.count else {return nil}
+            let codeIndex = program.index(program.startIndex, offsetBy: address)
+            return .init(program[codeIndex])
+        }
+    }
+    let testProgramSource = TestProgiramSource()
+    
+    let vm = BFVirtualMachine()
+    vm.dataSource = testProgramSource
+    vm.run()
+}
