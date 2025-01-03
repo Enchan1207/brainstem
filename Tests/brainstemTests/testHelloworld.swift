171e93902e5047158428ecb67ba7b93a07211e3d
Enchan <enchant0130@gmail.com>
2025-01-03 11:13:58 +0900

    [Modify] use XCTest instead of Testing for CI

--- Tests/brainstemTests/testHelloworld.swift
+++ Tests/brainstemTests/testHelloworld.swift
@@ -1,20 +1,23 @@
-import Testing
+import XCTest
 @testable import brainstem
 
-
-@Test func testRunHelloWorld() async throws {
-    class TestProgiramSource: BFProgramDataSource {
-        private let program = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."
-        
-        func opcode(at address: BFAddress) -> BFMachineOpcode? {
-            guard address < program.count else {return nil}
-            let codeIndex = program.index(program.startIndex, offsetBy: .init(address))
-            return .init(rawValue: program[codeIndex])
-        }
-    }
-    let testProgramSource = TestProgiramSource()
+final class testError: XCTestCase {
     
-    let vm = BFVirtualMachine()
-    vm.dataSource = testProgramSource
-    vm.run()
+    func testRunHelloWorld() throws {
+        class TestProgiramSource: BFProgramDataSource {
+            private let program = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."
+            
+            func opcode(at address: BFAddress) -> BFMachineOpcode? {
+                guard address < program.count else {return nil}
+                let codeIndex = program.index(program.startIndex, offsetBy: .init(address))
+                return .init(rawValue: program[codeIndex])
+            }
+        }
+        let testProgramSource = TestProgiramSource()
+        
+        let vm = BFVirtualMachine()
+        vm.dataSource = testProgramSource
+        vm.run()
+    }
+    
 }
