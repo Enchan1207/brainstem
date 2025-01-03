afb18bebbbf033a880c12157acb265fdceab3e2e
Enchan <enchant0130@gmail.com>
2025-01-03 10:21:56 +0900

    [Modify] testcase

--- Tests/brainfuck_implTests/brainfuck_implTests.swift
+++ Tests/brainfuck_implTests/brainfuck_implTests.swift
@@ -1,9 +1,6 @@
 import Testing
 @testable import brainfuck_impl
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-}
 
 @Test func testRunHelloWorld() async throws {
     class TestProgiramSource: BFProgramDataSource {
@@ -11,8 +8,8 @@ import Testing
         
         func opcode(at address: BFAddress) -> BFMachineOpcode? {
             guard address < program.count else {return nil}
-            let codeIndex = program.index(program.startIndex, offsetBy: address)
-            return .init(program[codeIndex])
+            let codeIndex = program.index(program.startIndex, offsetBy: .init(address))
+            return .init(rawValue: program[codeIndex])
         }
     }
     let testProgramSource = TestProgiramSource()
